628e88e17f230c37feec352540740933718ec7b1
AvaMarie Mendoza <amendoza@geo-comm.com>
2025-05-05 14:54:46 -0400

    Many updates

---
+++ translations.js
@@ -0,0 +1,90 @@
+const translations = {
+    en: {
+      title: "Portfolio - AvaMarie Mendoza",
+      welcome: "AvaMarie Mendoza",
+      intro: "Based in Michigan, primarily versed in remote work.",
+      nav: {
+        about: "About",
+        projects: "Projects",
+        certifications: "Certifications",
+        contact: "Contact",
+        conferences: "Conferences"
+      },
+      about: {
+        text: "Hello! Welcome to my portfolio. I am both fascinated and intrigued about learning anything related to geography. My specialized fields of interest are indoor mapping, vertical location services, programming using Python, and address standardization using MSAG and road centerline data. I have experience with ArcGIS Pro, ArcMap, AutoCAD, scan2CAD, Adobe Acrobat, Microsoft Excel, Airflow, and ArcGIS Online Applications. I’m an experienced geospatial professional and passionate problem solver. I thrive in the intersection of maps, data, and community.",
+        full: "Hello! Welcome to my portfolio. I am both fascinated and intrigued about learning anything related to geography. My specialized fields of interest are indoor mapping, vertical location services, programming using Python, and address standardization using MSAG and road centerline data. I have experience with ArcGIS Pro, ArcMap, AutoCAD, scan2CAD, Adobe Acrobat, Microsoft Excel, Airflow, and ArcGIS Online Applications. I’m an experienced geospatial professional and passionate problem solver. I thrive in the intersection of maps, data, and community.",
+        exp1: "Experience Builder",
+        exp2: "Dashboards",
+        exp3: "Field Maps Designer"
+      },
+      projects: {
+        title: "Projects",
+        text: "Things I've built!"
+      },
+      certifications: {
+        title: "My Certifications",
+        text: "Here’s a little bit about me. I’m learning web development!"
+      },
+      contact: {
+        title: "Contact Me"
+      },
+      conferences: {
+        title: "Conferences I've attended",
+        text: "In 2024, I attending the Annual Michigan 911 Technology Conference in Lansing Michiga as a representative for GeoComm. Here we made new connections with Michigan stakeholders and policy leaders, attended presentations for up-in-coming technologies and fundamentals of emergency response, and presented how GeoComm uses Z-axis and indoor mapping tehcnology to locate calls within a building faster. (Or even a corn silo!)"
+      },
+      gallery: {
+        title: "Gallery"
+      },
+      form: {
+        name: "Your Name",
+        email: "Your Email",
+        message: "Message",
+        send: "Send Message"
+      },
+      linkedin: "LinkedIn 💼"
+    },
+    es: {
+        title: "Portafolio de AvaMarie Mendoza",
+        welcome: "Bienvenidos a mi sitio web",
+        intro: "Esta es una página web sencilla construida con HTML y CSS.",
+        nav: {
+          about: "Acerca de",
+          projects: "Proyectos",
+          certifications: "Certificaciones",
+          contact: "Contacto",
+          conferences: "Conferencias"
+        },
+        about: {
+          text: "¡Hola! Bienvenidos a mi portafolio. Me fascina y intriga aprender todo lo relacionado con la geografía. Mis campos de interés especializados son el mapeo de interiores, los servicios de ubicación vertical, la programación con Python y la estandarización de direcciones utilizando MSAG y datos de líneas centrales de carreteras. Tengo experiencia con las aplicaciones ArcGIS Pro, ArcMap, AutoCAD, scan2CAD, Adobe Acrobat, Microsoft Excel, Airflow y ArcGIS Online. Soy un profesional geoespacial experimentado y un apasionado solucionador de problemas. Yo prospero en la intersección de los mapas, los datos y la comunidad.",
+          full: "¡Hola! Bienvenidos a mi portafolio. Me fascina y intriga aprender todo lo relacionado con la geografía. Mis campos de interés especializados son el mapeo de interiores, los servicios de ubicación vertical, la programación con Python y la estandarización de direcciones utilizando MSAG y datos de líneas centrales de carreteras. Tengo experiencia con las aplicaciones ArcGIS Pro, ArcMap, AutoCAD, scan2CAD, Adobe Acrobat, Microsoft Excel, Airflow y ArcGIS Online. Soy un profesional geoespacial experimentado y un apasionado solucionador de problemas. Yo prospero en la intersección de los mapas, los datos y la comunidad.",
+          exp1: "Constructor de Experiencias",
+          exp2: "Tableros",
+          exp3: "Diseñador de Mapas de Campo"
+        },
+        projects: {
+          title: "Proyectos",
+          text: "¡Cosas que he creado!"
+        },
+        certifications: {
+          title: "Mis Certificaciones",
+          text: "Aquí hay un poco sobre mí. ¡Estoy aprendiendo desarrollo web!"
+        },
+        contact: {
+          title: "Contáctame"
+        },
+        conferences: {
+          title: "Conferencias que he asistido",
+          text: "Aquí hay un poco sobre mí. ¡Estoy aprendiendo desarrollo web!"
+        },
+        gallery: {
+          title: "Galería"
+        },
+        form: {
+          name: "Tu Nombre",
+          email: "Tu Correo",
+          message: "Mensaje",
+          send: "Enviar Mensaje"
+        },
+        linkedin: "LinkedIn 💼"
+      }
+    };
