214bb57f6bcd5cc424363384cc6adfd1be6ae46d
AvaMarie Mendoza <amendoza@geo-comm.com>
2025-07-08 23:20:25 -0400

    change up

--- translations.js
+++ translations.js
@@ -2,15 +2,17 @@ const translations = {
     en: {
       title: "Portfolio - AvaMarie Mendoza",
       welcome: "AvaMarie Mendoza",
-      intro: "Based in Michigan, primarily versed in remote work.",
+      intro: "Based in Michigan 🌍",
       nav: {
         about: "About",
         projects: "Projects",
         certifications: "Certifications",
+        gallery:"Certificates",
         contact: "Contact",
         conferences: "Conferences"
       },
       about: {
+        title: "About Me",
         text: "Hello! Welcome to my portfolio. I am both fascinated and intrigued about learning anything related to geography. My specialized fields of interest are indoor mapping, vertical location services, programming using Python, and address standardization using MSAG and road centerline data. I have experience with ArcGIS Pro, ArcMap, AutoCAD, scan2CAD, Adobe Acrobat, Microsoft Excel, Airflow, and ArcGIS Online Applications. I’m an experienced geospatial professional and passionate problem solver. I thrive in the intersection of maps, data, and community.",
         full: "Hello! Welcome to my portfolio. I am both fascinated and intrigued about learning anything related to geography. My specialized fields of interest are indoor mapping, vertical location services, programming using Python, and address standardization using MSAG and road centerline data. I have experience with ArcGIS Pro, ArcMap, AutoCAD, scan2CAD, Adobe Acrobat, Microsoft Excel, Airflow, and ArcGIS Online Applications. I’m an experienced geospatial professional and passionate problem solver. I thrive in the intersection of maps, data, and community.",
         exp1: "Experience Builder",
@@ -19,21 +21,21 @@ const translations = {
       },
       projects: {
         title: "Projects",
-        text: "Things I've built!"
+        text: "Projects I have made myself."
       },
       certifications: {
         title: "My Certifications",
-        text: "Here’s a little bit about me. I’m learning web development!"
+        text: "Certifications such as GIS and GISP (coming soon)",
       },
       contact: {
         title: "Contact Me"
       },
       conferences: {
         title: "Conferences I've attended",
-        text: "In 2024, I attending the Annual Michigan 911 Technology Conference in Lansing Michiga as a representative for GeoComm. Here we made new connections with Michigan stakeholders and policy leaders, attended presentations for up-in-coming technologies and fundamentals of emergency response, and presented how GeoComm uses Z-axis and indoor mapping tehcnology to locate calls within a building faster. (Or even a corn silo!)"
+        text: "In 2024, I attended the Annual Michigan 911 Technology Conference in Lansing Michigan as a representative for GeoComm. Here we made new connections with Michigan stakeholders and policy leaders, attended presentations for up-in-coming technologies and fundamentals of emergency response, and presented how GeoComm uses Z-axis and indoor mapping tehcnology to locate calls within a building faster. (Or even a corn silo!)"
       },
       gallery: {
-        title: "Gallery"
+        title: "Certificates Earned"
       },
       form: {
         name: "Your Name",
@@ -44,17 +46,19 @@ const translations = {
       linkedin: "LinkedIn 💼"
     },
     es: {
-        title: "Portafolio de AvaMarie Mendoza",
-        welcome: "Bienvenidos a mi sitio web",
-        intro: "Esta es una página web sencilla construida con HTML y CSS.",
+        title: "Portafolio - AvaMarie Mendoza",
+        welcome: "AvaMarie Mendoza",
+        intro: "Soy de Michigan 🌍",
         nav: {
           about: "Acerca de",
           projects: "Proyectos",
           certifications: "Certificaciones",
+          gallery: "Certificados",
           contact: "Contacto",
           conferences: "Conferencias"
         },
         about: {
+          title: "Acerca De Mí",
           text: "¡Hola! Bienvenidos a mi portafolio. Me fascina y intriga aprender todo lo relacionado con la geografía. Mis campos de interés especializados son el mapeo de interiores, los servicios de ubicación vertical, la programación con Python y la estandarización de direcciones utilizando MSAG y datos de líneas centrales de carreteras. Tengo experiencia con las aplicaciones ArcGIS Pro, ArcMap, AutoCAD, scan2CAD, Adobe Acrobat, Microsoft Excel, Airflow y ArcGIS Online. Soy un profesional geoespacial experimentado y un apasionado solucionador de problemas. Yo prospero en la intersección de los mapas, los datos y la comunidad.",
           full: "¡Hola! Bienvenidos a mi portafolio. Me fascina y intriga aprender todo lo relacionado con la geografía. Mis campos de interés especializados son el mapeo de interiores, los servicios de ubicación vertical, la programación con Python y la estandarización de direcciones utilizando MSAG y datos de líneas centrales de carreteras. Tengo experiencia con las aplicaciones ArcGIS Pro, ArcMap, AutoCAD, scan2CAD, Adobe Acrobat, Microsoft Excel, Airflow y ArcGIS Online. Soy un profesional geoespacial experimentado y un apasionado solucionador de problemas. Yo prospero en la intersección de los mapas, los datos y la comunidad.",
           exp1: "Constructor de Experiencias",
@@ -63,21 +67,21 @@ const translations = {
         },
         projects: {
           title: "Proyectos",
-          text: "¡Cosas que he creado!"
+          text: "Proyectos que he hecho yo mismo. "
         },
         certifications: {
           title: "Mis Certificaciones",
-          text: "Aquí hay un poco sobre mí. ¡Estoy aprendiendo desarrollo web!"
+          text: "Certificaciones como a GIS y GISP (próximamente)",
         },
         contact: {
           title: "Contáctame"
         },
         conferences: {
           title: "Conferencias que he asistido",
-          text: "Aquí hay un poco sobre mí. ¡Estoy aprendiendo desarrollo web!"
+          text: "Asistí a la Conferencia Anual de Teconolía Michigan 911 en Lansing Michigan como respresante de GeoComm. Aquí hicimos nuevas conexiones con las partes interesadas y los líderes políticos de Michigan, asistimos a presentaciones sobre las nuevas tecnologías y los fundamentos de la respuesta a emergencias, y presentamos cómo GeoComm utiliza el eje Z y la tehcnología de cartografía en interiores para localizar las llamadas dentro de un edificio más rápido. (¡O incluso un silo de maíz! "
         },
         gallery: {
-          title: "Galería"
+          title: "Los Certificados"
         },
         form: {
           name: "Tu Nombre",
